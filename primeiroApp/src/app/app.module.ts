c81b5f671ec40f8b6b971ba85d16bdddde5bca19
Jefferson de Assis <jeffersondeassis13022000@gmail.com>
2020-05-07 02:32:18 +0000

    criação de modulo / index.ts

--- primeiroApp/src/app/app.module.ts
+++ primeiroApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {ContarModule} from './contar/contar.module';
+import {ContarModule} from './contar';
 
 @NgModule({
   declarations: [
