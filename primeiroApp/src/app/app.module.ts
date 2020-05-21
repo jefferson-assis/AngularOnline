44750692e058d147961aa3b8954fce14dd2cca98
Jefferson de Assis <jeffersondeassis13022000@gmail.com>
2020-05-21 19:12:13 +0000

    Criando rotas/Aula 6

--- primeiroApp/src/app/app.module.ts
+++ primeiroApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import {ContarModule} from './contar';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {ContarModule} from './contar';
   ],
   imports: [
     BrowserModule,
-    ContarModule
+    ContarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- primeiroApp/src/app/contar/index.ts
+++ primeiroApp/src/app/contar/index.ts
@@ -1,2 +1,3 @@
 export * from './contar.module';
 export * from './services';
+export * from './contar-routing.module';
